b0e89e9c9710d7eaeb4cdc148cd3387dbad8e040
pk-vishnu <vishnuvpk05@gmail.com>
2025-11-11 12:35:27 +0530

    fix(infra): enable s3 versioning, remove deprecated acl args, bucketOwnerEnforced used instead

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,10 +1,6 @@
+# Input S3 bucket
 resource "aws_s3_bucket" "input_bucket" {
   bucket_prefix = var.input_bucket_prefix
-  acl           = "private"
-
-  versioning {
-    enabled = true
-  }
 
   tags = {
     Name = "media-input"
@@ -12,16 +8,46 @@ resource "aws_s3_bucket" "input_bucket" {
   }
 }
 
+# Output S3 bucket
 resource "aws_s3_bucket" "output_bucket" {
   bucket_prefix = var.output_bucket_prefix
-  acl           = "private"
-
-  versioning {
-    enabled = true
-  }
 
   tags = {
     Name = "media-output"
     Env  = "dev"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "input_ownership" {
+  bucket = aws_s3_bucket.input_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "output_ownership" {
+  bucket = aws_s3_bucket.output_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+# Enable versioning for input bucket
+resource "aws_s3_bucket_versioning" "input_versioning" {
+  bucket = aws_s3_bucket.input_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable versioning for output bucket
+resource "aws_s3_bucket_versioning" "output_versioning" {
+  bucket = aws_s3_bucket.output_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
